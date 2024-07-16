a7db1b9e9a323458671ffa21ffae58cce77d7d82
José Luis <73161066+JoseLuisCobo17@users.noreply.github.com>
2024-07-16 14:37:52 +0200

    Add files via upload

---
+++ Spypro-api/scripts/mongo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Detener cualquier instancia en ejecución de mongod
+sudo killall -9 mongod
+
+# Iniciar mongod con sudo para asegurar permisos correctos
+sudo mongod --dbpath /var/lib/mongodb --logpath /var/log/mongodb/mongod.log --fork
+
+# Abrir una nueva terminal y ejecutar api.sh
+gnome-terminal -- bash -c "/home/jose_luis/Escritorio/tfg/Spypro-api/scripts/api.sh; exec bash"
