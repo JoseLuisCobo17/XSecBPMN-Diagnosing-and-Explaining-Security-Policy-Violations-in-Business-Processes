24d1e63f445a8199bfb1a9417a5950352200721d
José Luis <73161066+JoseLuisCobo17@users.noreply.github.com>
2024-07-16 14:43:20 +0200

    Create ModelerSpec.js

---
+++ Spypro-editor/test/spec/ModelerSpec.js
@@ -0,0 +1,175 @@
+import {
+  addStyle
+} from '../Helper';
+
+addStyle(`
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body {
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    padding: 10px;
+  }
+
+  .icon-red {
+    background: red !important;
+  }
+
+  .bjs-container {
+    border: solid 2px #CCC;
+    margin-bottom: 10px;
+  }
+`);
+
+import Modeler from 'bpmn-js/lib/Modeler';
+
+import resizeAllModule from '../../lib/resize-all-rules';
+
+import colorPickerModule from '../../lib/color-picker';
+
+import nyanDrawModule from '../../lib/nyan/draw';
+import nyanPaletteModule from '../../lib/nyan/palette';
+
+
+describe('modeler / nyan version', function() {
+
+  var diagram = require('./diagram.bpmn');
+
+
+  function withModeler(config, fn) {
+
+    return function(done) {
+
+      var modeler = new Modeler(config);
+
+      modeler.attachTo('body');
+
+      modeler.importXML(diagram).then(
+        () => modeler.invoke(fn)
+      ).then(done, done);
+    };
+  }
+
+
+  describe('just modeler', function() {
+
+    function inject(fn) {
+      return withModeler({}, fn);
+    }
+
+
+    it('should allow me to render a process', inject(function() {
+
+    }));
+
+  });
+
+
+  describe('custom rules (resize all)', function() {
+
+    function inject(fn) {
+
+      var config = {
+        additionalModules: [ resizeAllModule ]
+      };
+
+      return withModeler(config, fn);
+    }
+
+
+    it('should allow to resize all elements', inject(function() {
+
+    }));
+
+  });
+
+
+  describe('custom colors', function() {
+
+    function inject(fn) {
+
+      var config = {
+        additionalModules: [ colorPickerModule ]
+      };
+
+      return withModeler(config, fn);
+    }
+
+
+    it('should allow to resize all elements', inject(function() {
+
+    }));
+
+  });
+
+
+  describe('draw', function() {
+
+    function inject(fn) {
+
+      var config = {
+        additionalModules: [ nyanDrawModule ]
+      };
+
+      return withModeler(config, fn);
+    }
+
+
+    it('should render cat', inject(function() {
+
+    }));
+
+  });
+
+
+  describe('palette', function() {
+
+    function inject(fn) {
+
+      var config = {
+        additionalModules: [ nyanPaletteModule ]
+      };
+
+      return withModeler(config, fn);
+    }
+
+
+    it('should create cat from palette', inject(function() {
+
+    }));
+
+  });
+
+
+  describe('combined', function() {
+
+    function inject(fn) {
+
+      var config = {
+        additionalModules: [
+          nyanPaletteModule,
+          nyanDrawModule,
+          colorPickerModule,
+          resizeAllModule
+        ],
+        keyboard: {
+          bindTo: document.body
+        }
+      };
+
+      return withModeler(config, fn);
+    }
+
+
+    it('should create cat from palette', inject(function() {
+
+    }));
+
+  });
+});
