d16c8fa5a40aa9ddc425d672661db82cb32c6f32
José Luis <73161066+JoseLuisCobo17@users.noreply.github.com>
2024-07-16 14:30:25 +0200

    Create security.model.js

---
+++ Spypro-api/app/models/security.model.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var SecuritySchema = mongoose.Schema({
+    id_model: { type: String, required: true },
+    id_bpmn: { type: String, required: true, unique:true },
+    BoD: {type: Boolean, default: false},
+    SoD: {type: Boolean, default: false},
+    UoC: {type: Boolean, default: false},
+    SubTasks: [String],
+    Nu: Number,
+    Mth: Number,
+    P: Number,
+    User: String,
+    Log: String
+
+});
+
+module.exports = mongoose.model('Security', SecuritySchema);
