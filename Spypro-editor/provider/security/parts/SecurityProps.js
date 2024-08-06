ca6ffe31f8cc4f10e1351a166b9b84f348189134
[[JoseLuisCobo17]] <[[josecobari@alum.us.es]]>
2024-08-06 10:07:24 +0200

    Funciona correctamente boton BoD(SIN BD)

--- Spypro-editor/provider/security/parts/SecurityProps.js
+++ Spypro-editor/provider/security/parts/SecurityProps.js
@@ -1,22 +1,19 @@
 import { html } from 'htm/preact';
-
-import { CheckboxEntry, isCheckboxEntryEdited } from '@bpmn-io/properties-panel';
+import { CheckboxEntry } from '@bpmn-io/properties-panel';
 import { useService } from 'bpmn-js-properties-panel';
 
 export default function(element) {
   return [
     {
-      id: 'spell',
+      id: 'BoD',
       element,
-      component: Spell,
-      isEdited: () => {
-        return isCheckboxEntryEdited() ? true : false;
-      }
+      component: BoDFunction,
+      isEdited: isCheckboxEntryEdited
     }
   ];
 }
 
-function Spell(props) {
+function BoDFunction(props) {
   const { element, id } = props;
 
   const modeling = useService('modeling');
@@ -24,23 +21,39 @@ function Spell(props) {
   const debounce = useService('debounceInput');
 
   const getValue = () => {
-    return element.businessObject.spell || false;
+    if (!element || !element.businessObject) {
+      return false; // Valor predeterminado si businessObject no está definido
+    }
+    const value = element.businessObject.Bod;
+    console.log('Current BoD value (getValue):', value); // Log para depuración
+    return value === true; // Asegurarse de que devuelve un booleano
   };
 
   const setValue = value => {
-    return modeling.updateProperties(element, {
-      spell: value
+    if (!element || !element.businessObject) {
+      return; // Salir si businessObject no está definido
+    }
+    console.log('Setting BoD to (setValue):', value); // Log para depuración
+    modeling.updateProperties(element, {
+      Bod: value
     });
   };
 
   return html`<${CheckboxEntry}
-    id=${ id }
-    element=${ element }
-    description=${ translate('Apply a black magic spell') }
-    label=${ translate('Spell') }
-    getValue=${ getValue }
-    setValue=${ setValue }
-    debounce=${ debounce }
-    tooltip=${ translate('Check available spells in the spellbook.') }
+    id=${id}
+    element=${element}
+    label=${translate('BoD')}
+    getValue=${getValue}
+    setValue=${setValue}
+    debounce=${debounce}
+    tooltip=${translate('Check available spells in the spellbook.')}
   />`;
+}
+
+function isCheckboxEntryEdited(element) {
+  if (!element || !element.businessObject) {
+    return false;
+  }
+  const propValue = element.businessObject.Bod;
+  return typeof propValue !== 'undefined';
 }
