81de24e14521f6ceaf5db1392636e8a6315f7a36
[[JoseLuisCobo17]] <[[josecobari@alum.us.es]]>
2024-09-23 11:17:11 +0200

    Vista de botones en html

--- Spypro-editor/webpack.config.js
+++ Spypro-editor/webpack.config.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
-
+const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { DefinePlugin } = require('webpack');
 
@@ -7,8 +7,7 @@ const { DefinePlugin } = require('webpack');
 module.exports = (env, argv) => {
 
   const mode = argv.mode || 'development';
-
-  const devtool = mode === 'development' ? 'eval-source-map' : 'source-map';
+  const isDevMode = mode === 'development';
 
   return {
     mode,
@@ -18,13 +17,29 @@ module.exports = (env, argv) => {
     },
     output: {
       filename: 'dist/[name].js',
-      path: __dirname + '/example'
+      path: path.resolve(__dirname, 'example'),
+      clean: true // Limpia los archivos viejos antes de generar nuevos bundles
     },
     module: {
       rules: [
         {
           test: /\.bpmn$/,
-          type: 'asset/source'
+          type: 'asset/source' // Permite importar archivos .bpmn como cadenas de texto
+        },
+        {
+          test: /\.json$/,
+          type: 'json', // Para manejar archivos JSON (como moddle descriptors)
+          parser: {
+            parse: JSON.parse
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader'] // Para manejar archivos CSS
+        },
+        {
+          test: /\.less$/,
+          use: [ 'style-loader', 'css-loader', 'less-loader'] // Para manejar archivos Less (si usas Less)
         }
       ]
     },
@@ -40,7 +55,14 @@ module.exports = (env, argv) => {
         'process.env.TOKEN_SIMULATION_VERSION': JSON.stringify(require('./package.json').version)
       })
     ],
-    devtool
+    devtool: isDevMode ? 'eval-source-map' : 'source-map', // Source maps más rápidos en desarrollo
+    devServer: {
+      static: path.join(__dirname, 'example'), // Carpeta donde buscará archivos estáticos
+      compress: true,
+      port: 9000, // Puerto para tu servidor de desarrollo
+      hot: true, // Habilita hot reloading para desarrollo
+      open: true // Abre automáticamente en el navegador
+    }
   };
 
 };
