521f7ec61df2dff61c5f597ea6bc40599b1cfb3a
JoseLuisCobo17 <joscobari@alum.us.es>
2025-01-13 12:36:58 +0100

    Fix: error al importar UserWithoutRoleList

--- Modeler/provider/model/parts/ParticipantWithoutLaneProps.js
+++ Modeler/provider/model/parts/ParticipantWithoutLaneProps.js
@@ -106,9 +106,11 @@ function userWithoutRoleFunction(props) {
     if (!element || !element.businessObject) {
       return [];
     }
-    return element.businessObject.userWithoutRole || [];
-    console.log(element.businessObject.userWithoutRole);
-  };
+  
+    // Asegurar que el valor sea un array
+    const userWithoutRole = element.businessObject.userWithoutRole;
+    return Array.isArray(userWithoutRole) ? userWithoutRole : (userWithoutRole ? [userWithoutRole] : []);
+  };  
 
   const updateUserWithoutRole = (index, value) => {
     const userWithoutRoleList = getUserWithoutRoleList();
